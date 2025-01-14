17c2f401640a078853dd19e8ad8ebc1ec29575e0
rasmi-inspiring <rasmi@inspiringlab.com.np>
2025-01-14 13:49:34 +0545

    FE-StreamLitApp: Added exception handling and logic to create database tables during application starting

--- app/main.py
+++ app/main.py
@@ -4,18 +4,39 @@ from services.login_func import login
 from services.submit_form import submit_form_page
 from services.view_data import view_data_page
 
+from database import engine, Base
+from sqlalchemy.exc import OperationalError
+from models import FormData, FileData, User
+import os
+
+
+def create_tables():
+    try:
+        Base.metadata.create_all(bind=engine)
+    except OperationalError as e:
+        st.error(f"An error occured while running the application.")
+
+
+# Set the page layout to 'wide'
+st.set_page_config(page_title="Policy Data Collection", layout="wide")
+
 
 def main_content():
-    st.title("Policy Data Collection")
+    try:
+        create_tables()
+        st.title("Policy Data Collection")
 
-    # Create tabs for different functionalities
-    tabs = st.tabs(["Add Data", "View Data"])
+        # Create tabs for different functionalities
+        tabs = st.tabs(["Add Data", "View Data"])
 
-    with tabs[0]:
-        submit_form_page()
+        with tabs[0]:
+            submit_form_page()
 
-    with tabs[1]:
-        view_data_page()
+        with tabs[1]:
+            view_data_page()
+
+    except Exception as e:
+        st.error("An error occured while trying to load the application.")
 
 
 def main():
@@ -44,7 +65,7 @@ def main():
                 text-align: center;
                 font-weight: bold;
                 margin-top: 70px;
-                margin-bottom: 20px; 
+                margin-bottom: 20px;
             }
             .subheader {
                 font-size: 25px;
