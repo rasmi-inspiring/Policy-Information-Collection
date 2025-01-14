85add5d811899bccd202cc3a29bf3469a24c7a59
rasmi-inspiring <rasmi@inspiringlab.com.np>
2025-01-14 13:48:17 +0545

    FE-StreamlitApp: Modified UI, added exception handling, and updated other logics as needed

--- app/services/login_func.py
+++ app/services/login_func.py
@@ -15,70 +15,76 @@ def get_user_by_username(username: str, db: Session):
 # Function to handle the login logic
 def login():
 
-    # Check if the user is already logged in
-    if "logged_in" not in st.session_state:
-        st.session_state.logged_in = False
+    try:
+        # Check if the user is already logged in
+        if "logged_in" not in st.session_state:
+            st.session_state.logged_in = False
 
-    with st.sidebar:
-        image_path = os.path.join("app", "images", "ilab.png")
+        with st.sidebar:
+            image_path = os.path.join("app", "images", "ilab.png")
 
-        # Check if the image exists
-        if os.path.exists(image_path):
-            # Create a centered column
-            col = st.columns([2, 6, 0.4])
-            with col[1]:
-                st.image(
-                    image_path, use_container_width=False, width=170
-                )  # Adjust width as needed
-        else:
-            st.warning("Image not found!")
+            # Check if the image exists
+            if os.path.exists(image_path):
+                # Create a centered column
+                col = st.columns([2, 6, 0.4])
+                with col[1]:
+                    st.image(
+                        image_path, use_container_width=False, width=170
+                    )  # Adjust width as needed
+            else:
+                st.warning("Image not found!")
 
-        # # Show login form if not logged in
-        if not st.session_state.logged_in:
+            # # Show login form if not logged in
+            if not st.session_state.logged_in:
 
-            st.markdown(
-                """
-                        <style>
-                            .custom-header {
-                                text-align: center;
-                                font-size: 32px;
-                                font-weight: bold;
-                                margin-top: 10px;
-                                margin-bottom: 10px;
-                            }
-                        </style>
-                        <div class="custom-header">Login</div>
-                    """,
-                unsafe_allow_html=True,
-            )
-            username = st.text_input("Username", key="username_input")
-            password = st.text_input("Password", type="password", key="password_input")
+                st.markdown(
+                    """
+                            <style>
+                                .custom-header {
+                                    text-align: center;
+                                    font-size: 32px;
+                                    font-weight: bold;
+                                    margin-top: 10px;
+                                    margin-bottom: 10px;
+                                }
+                            </style>
+                            <div class="custom-header">Login</div>
+                        """,
+                    unsafe_allow_html=True,
+                )
+                username = st.text_input("Username", key="username_input")
+                password = st.text_input(
+                    "Password", type="password", key="password_input"
+                )
 
-            if st.button("Login"):
-                # Initialize the database session
-                db = SessionLocal()
+                if st.button("Login"):
+                    # Initialize the database session
+                    db = SessionLocal()
 
-                # Fetch the user from the database
-                user = get_user_by_username(username, db)
+                    # Fetch the user from the database
+                    user = get_user_by_username(username, db)
 
-                if user and user.password == password:
-                    st.session_state.logged_in = True
-                    st.session_state.username = username
-                    st.success("Login successful!")
-                    st.rerun()
-                else:
-                    st.error("Incorrect username or password")
+                    if user and user.password == password:
+                        st.session_state.logged_in = True
+                        st.session_state.username = username
+                        st.success("Login successful!")
+                        st.rerun()
+                    else:
+                        st.error("Incorrect username or password")
 
-                db.close()  # Close the session after use
-        else:
-            col = st.columns([2.5, 6, 1])
-            with col[1]:
-                st.header(f"Welcome, {st.session_state.username}!")
+                    db.close()  # Close the session after use
+            else:
+                col = st.columns([2.5, 6, 1])
+                with col[1]:
+                    st.header(f"Welcome, {st.session_state.username}!")
 
-            # Create a centered column
-            col = st.columns([23, 30, 10])
-            with col[1]:
-                if st.button("Logout"):
-                    st.session_state.logged_in = False
-                    st.session_state.username = None
-                    st.rerun()
+                # Create a centered column
+                col = st.columns([23, 30, 10])
+                with col[1]:
+                    if st.button("Logout"):
+                        st.session_state.logged_in = False
+                        st.session_state.username = None
+                        st.rerun()
+
+    except Exception as e:
+        st.error("An error occured during login.")
--- app/services/view_data.py
+++ app/services/view_data.py
@@ -4,54 +4,177 @@ from sqlalchemy.orm import Session
 from database import engine
 
 
-# View Data Page
 def view_data_page():
-    st.title("View Data")
-    with Session(engine) as session:
-        form_data_entries = session.query(FormData).all()
-        st.subheader(f"Total Queries: {len(form_data_entries)}")
 
-        for i, form_data in enumerate(form_data_entries, 1):
-            with st.expander(f"Query {i}: {form_data.query}"):
-                st.write(f"**ID**: {form_data.id}")
-                st.write(f"**Category**: {form_data.category}")
-                st.write(f"**Related Office**: {form_data.related_office}")
-                st.write(
-                    f"**Related Secondary Office 1**: {form_data.related_secondary_office_1}"
-                )
-                st.write(
-                    f"**Related Secondary Office 2**: {form_data.related_secondary_office_2}"
-                )
-                st.write(f"**Other Offices**: {form_data.other_offices}")
-                st.write(f"**Topic**: {form_data.topic}")
-                st.write(f"**Query**: {form_data.query}")
-                st.write(f"**Response**:")
-                st.markdown(form_data.response, unsafe_allow_html=True)
-                st.write(f"**Attachments**: {form_data.attachments}")
-                st.write(f"**Links**: {form_data.links}")
-                st.write(
-                    f"**Related Law/Policy/Act**: {form_data.related_law_policy_act}"
-                )
-                st.write(f"**Validated By Name**: {form_data.validated_by_name}")
-                st.write(f"**Validated By Office**: {form_data.validated_by_office}")
-                st.write(
-                    f"**Validated By Position**: {form_data.validated_by_position}"
-                )
-                st.write(f"**Validated By Address**: {form_data.validated_by_address}")
+    try:
+        st.title("View Data")
 
-                file_data_list = (
-                    session.query(FileData).filter_by(form_data_id=form_data.id).all()
-                )
-                if file_data_list:
-                    st.write("**Uploaded Files:**")
-                    for file_data in file_data_list:
-                        with open(file_data.file_path, "rb") as f:
-                            file_bytes = f.read()
+        # Increase font size for the title
+        st.markdown(
+            "<h1 style='font-size: 32px;'>View Data</h1>", unsafe_allow_html=True
+        )
 
-                        st.download_button(
-                            label=f"Download {file_data.file_name}",
-                            data=file_bytes,
-                            file_name=file_data.file_name,
-                            mime="application/octet-stream",
-                            key=f"download_{file_data.id}",
+        with Session(engine) as session:
+            form_data_entries = session.query(FormData).all()
+            st.subheader(f"Total Queries: {len(form_data_entries)}")
+
+            for i, form_data in enumerate(form_data_entries, 1):
+                with st.expander(f"## Query {i}: {form_data.query}"):
+
+                    # Function to display key-value pairs without boxes and custom font size
+                    def display_key_value(
+                        key, value, inline=False, margin_bottom=10, font_size="16px"
+                    ):
+
+                        if value:
+                            st.markdown(
+                                f"<div style='margin-bottom: {margin_bottom}px; font-size: {font_size};'>"
+                                f"<span style='font-weight: bold;'>{key}:</span> "
+                                f"<span>{value}</span></div>",
+                                unsafe_allow_html=True,
+                            )
+
+                    # Section 1: ID, Category and Offices
+                    st.markdown(
+                        "<h3 style='font-size: 24px;'>General Information</h3>",
+                        unsafe_allow_html=True,
+                    )
+
+                    display_key_value(
+                        "Category",
+                        form_data.category,
+                        margin_bottom=20,
+                        font_size="18px",
+                    )
+
+                    col1, col2 = st.columns([1, 1])
+                    with col1:
+                        display_key_value(
+                            "Related Office",
+                            form_data.related_office,
+                            margin_bottom=20,
+                            font_size="18px",
                         )
+                    with col2:
+                        display_key_value(
+                            "Secondary Office 1",
+                            form_data.related_secondary_office_1,
+                            margin_bottom=20,
+                            font_size="18px",
+                        )
+                    col1, col2 = st.columns([1, 1])
+                    with col1:
+                        display_key_value(
+                            "Secondary Office 2",
+                            form_data.related_secondary_office_2,
+                            margin_bottom=20,
+                            font_size="18px",
+                        )
+                    with col2:
+                        display_key_value(
+                            "Other Offices",
+                            form_data.other_offices,
+                            margin_bottom=20,
+                            font_size="18px",
+                        )
+                    st.markdown("---")
+
+                    # Section 2: Topic, Query, and Response
+                    st.markdown(
+                        "<h3 style='font-size: 24px;'>Details</h3>",
+                        unsafe_allow_html=True,
+                    )
+                    display_key_value(
+                        "Topic", form_data.topic, margin_bottom=20, font_size="18px"
+                    )
+                    display_key_value(
+                        "Query", form_data.query, margin_bottom=20, font_size="18px"
+                    )
+                    display_key_value(
+                        "Response",
+                        form_data.response,
+                        margin_bottom=20,
+                        font_size="18px",
+                    )
+                    st.markdown("---")
+
+                    st.markdown(
+                        "<h3 style='font-size: 24px;'>Attachments & References</h3>",
+                        unsafe_allow_html=True,
+                    )
+                    display_key_value(
+                        "Attachments",
+                        form_data.attachments,
+                        margin_bottom=20,
+                        font_size="18px",
+                    )
+                    display_key_value(
+                        "Links", form_data.links, margin_bottom=20, font_size="18px"
+                    )
+                    display_key_value(
+                        "Related Law/Policy/Act",
+                        form_data.related_law_policy_act,
+                        margin_bottom=20,
+                        font_size="18px",
+                    )
+
+                    # Section 4: Validation Info
+                    st.markdown("---")
+                    col1, col2 = st.columns([1, 1])
+                    with col1:
+                        display_key_value(
+                            "Name",
+                            form_data.validated_by_name,
+                            margin_bottom=20,
+                            font_size="18px",
+                        )
+                    with col2:
+                        display_key_value(
+                            "Office",
+                            form_data.validated_by_office,
+                            margin_bottom=20,
+                            font_size="18px",
+                        )
+
+                    col1, col2 = st.columns([1, 1])
+                    with col1:
+                        display_key_value(
+                            "Position",
+                            form_data.validated_by_position,
+                            margin_bottom=20,
+                            font_size="18px",
+                        )
+                    with col2:
+                        display_key_value(
+                            "Address",
+                            form_data.validated_by_address,
+                            margin_bottom=20,
+                            font_size="18px",
+                        )
+
+                    st.markdown("---")
+
+                    file_data_list = (
+                        session.query(FileData)
+                        .filter_by(form_data_id=form_data.id)
+                        .all()
+                    )
+                    if file_data_list:
+                        st.markdown(
+                            "<h3 style='font-size: 24px;'>Uploaded Files</h3>",
+                            unsafe_allow_html=True,
+                        )
+                        for file_data in file_data_list:
+                            with open(file_data.file_path, "rb") as f:
+                                file_bytes = f.read()
+
+                            st.download_button(
+                                label=f"Download {file_data.file_name}",
+                                data=file_bytes,
+                                file_name=file_data.file_name,
+                                mime="application/octet-stream",
+                                key=f"download_{file_data.id}",
+                            )
+
+    except Exception as e:
+        st.error("An error occured while trying to load view data page.")
